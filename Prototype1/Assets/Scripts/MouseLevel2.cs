57d8eadbcef0522ed4098e418559dae7d5150913
alylahaise <98407961+alylahaise@users.noreply.github.com>
2024-04-15 20:09:05 -0400

    April 15 Level2 Hallway
    - constructed the layout of level 2: hallway
    - doors all set: player will teleport to the door from center of room when clicked on
    - click on center of room to teleport back
    - window area: no teleport yet idk if we want on
    
    Next steps: implement enemies

---
+++ Prototype1/Assets/Scripts/MouseLevel2.cs
@@ -0,0 +1,113 @@
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class MouseLevel2 : MonoBehaviour
+{
+
+    public Camera FPcamera;
+    public LayerMask interactable;
+    public GameObject player;
+    public GameObject center;
+    public GameObject door2;
+    public GameObject door1;
+
+    public bool isInRoom;
+    public bool isAtDoor;
+    public bool doorClosed;
+    Vector3 door2Coordinates;
+    Vector3 door1Coordinates;
+    Vector3 centerCoordinates;
+
+
+    Collider centerCollider;
+
+    Animator animDoor2;
+    Animator animDoor1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isInRoom = true;
+        isAtDoor = false;
+        doorClosed = false;
+        centerCoordinates = new Vector3(0f, 0.50f, 0f);
+        door2Coordinates = new Vector3(8.0f, 0.58f, 1.2f);
+        door1Coordinates = new Vector3(-8.0f, 0.58f, 0f);
+        centerCollider = center.GetComponent<Collider>();
+        animDoor2 = door2.GetComponent<Animator>();
+        animDoor1 = door1.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Ray ray = FPcamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, 200, interactable))
+        {
+
+            transform.position = raycastHit.point;
+            if (Input.GetMouseButtonUp(0) && doorClosed)
+            {
+                doorClosed = false;
+                animDoor2.SetBool("doorClosed", false);
+                animDoor1.SetBool("doorClosed", false);
+            }
+            if (Input.GetMouseButtonDown(0))
+            {
+                // DOOR 2
+                Debug.Log(raycastHit.transform.name);
+                // if hits a door
+                if (raycastHit.transform.CompareTag("Door2") && isInRoom)
+                {
+                    isInRoom = false;
+                    isAtDoor = true;
+                    animDoor2.SetBool("isAtDoor", true);
+                    player.transform.position = door2Coordinates;
+                    centerCollider.enabled = true;
+                }
+                else if (raycastHit.transform.CompareTag("Door2") && !isInRoom && !doorClosed)
+                {
+                    doorClosed = true;
+                    animDoor2.SetBool("doorClosed", true);
+                }
+                // DOOR 1
+                else if (raycastHit.transform.CompareTag("Door1") && isInRoom)
+                {
+                    isInRoom = false;
+                    isAtDoor = true;
+                    animDoor1.SetBool("isAtDoor", true);
+                    player.transform.position = door1Coordinates;
+                    centerCollider.enabled = true;
+                }
+                else if (raycastHit.transform.CompareTag("Door1") && !isInRoom && !doorClosed)
+                {
+                    doorClosed = true;
+                    animDoor1.SetBool("doorClosed", true);
+                }
+                else if (raycastHit.transform.CompareTag("Center"))
+                {
+                    isInRoom = true;
+                    isAtDoor = false;
+                    centerCollider.enabled = false;
+                    animDoor2.SetBool("isAtDoor", false);
+                    animDoor1.SetBool("isAtDoor", false);
+                    player.transform.position = centerCoordinates;
+
+                }
+            }
+
+        }
+
+        Debug.DrawRay(ray.origin, ray.direction * raycastHit.distance, Color.red);
+
+
+
+    }
+
+
+
+}
