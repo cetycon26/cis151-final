727b08231d45544b603bcaa3566db4b4281c5366
Christian Concepcion <70904084+cetycon26@users.noreply.github.com>
2024-04-30 23:20:03 -0400

    Camera unfreeze on Scene Startup

--- Prototype1/Assets/Scripts/SceneController.cs
+++ Prototype1/Assets/Scripts/SceneController.cs
@@ -9,7 +9,8 @@ public class SceneController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        CameraController.lookSpeed = 1.0f;
+        BodyController.lookSpeed = 1.0f;
     }
 
     // Update is called once per frame
